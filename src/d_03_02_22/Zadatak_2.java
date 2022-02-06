258558160bcb0f5da8264baa4e30f79463f79122
Nada83Nikolic <nada83nikolic@gmail.com>
2022-02-06 15:12:10 +0100

    Zadatak_2

--- src/d_03_02_22/Zadatak_2.java
+++ src/d_03_02_22/Zadatak_2.java
@@ -1,9 +1,33 @@
 package d_03_02_22;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Zadatak_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "driver-lib/chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		driver.get("https://www.wikipedia.org/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		
+		List<WebElement> linkovi = driver.findElements(By.xpath("//*[@class='link-box']"));
+
+		for (int i = 0; i < linkovi.size(); i++) {
+			
+			js.executeScript("window.open(arguments[0]);", linkovi.get(i));
+		}
+		Thread.sleep(10000);
+		driver.quit();
+	
 
 	}
 

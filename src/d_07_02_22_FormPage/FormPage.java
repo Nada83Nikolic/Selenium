7500442ee0bc42d3c14f27297dc363a28054ff5e
Nada83Nikolic <nada83nikolic@gmail.com>
2022-02-12 16:47:18 +0100

    FormTest_07_02_22

--- src/d_07_02_22_FormPage/FormPage.java
+++ src/d_07_02_22_FormPage/FormPage.java
@@ -1,5 +1,84 @@
 package d_07_02_22_FormPage;
 
-public class FormPage {
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FormPage {
+	
+	private WebDriver driver;
+	
+	public FormPage(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	public WebElement getInputName() {
+		return driver.findElement(By.id("first-name"));
+	}
+
+	
+	public WebElement getInputGender(String gender) {
+		
+	return	driver.findElement(By.xpath("//*[@name='gender'][@value='"+gender+"']"));
+		
+	
+	}
+	
+	
+	
+	public WebElement getInputDateOfBirth() {
+		return driver.findElement(By.id("dob"));
+	}
+	
+	
+	public WebElement getInputEmail() {
+		return driver.findElement(By.id("email"));
+	}
+	
+	public WebElement getCheckBoxInputs(String checkBoxInput) {
+		return driver.findElement(By.xpath("//*[@type='checkbox'][@value='"+ checkBoxInput +"'] "));
+	}
+	public Select Role() {
+		Select select = new Select(driver.findElement(By.id("role")));
+		return select;
+	}
+	
+	//metodu koja vraca da li su podaci uspesno sacuvani, 
+	//tako sto proverava da li element koji nosi poruku za atribut style
+	//ima vrednost "visibility: visible"
+
+	
+	
+	public WebElement getInputComment() {
+		return driver.findElement(By.id("comment"));
+	}
+	
+	public WebElement getSubmitButton() {
+		return driver.findElement(By.id("submit"));
+	}
+	public boolean daLiSuPodaciSacuvani() {
+	
+	//	driver.findElement(By.xpath("//*[@class='message-element poruka'][@style='visibility: visible']"));
+		//return true;
+		
+			boolean tuJe = true;
+			List<WebElement> list = driver.findElements(By.xpath("//div[@style='visibility: visible']"));
+			if (list.size() == 0) {
+				tuJe = false;
+			}
+			return tuJe;
+		
+	}
+	public void scrollTo()
+	{JavascriptExecutor js=(JavascriptExecutor) driver;
+		WebElement commentField=driver.findElement(By.xpath("//*[@class='form-group'][5]"));
+		js.executeAsyncScript("arguments[0].scrollIntoView();", commentField);
+		
+	}
+	
 }
+
--- src/d_07_02_22_FormTest/FormTest.java
+++ src/d_07_02_22_FormTest/FormTest.java
@@ -1,5 +1,65 @@
 package d_07_02_22_FormTest;
 
-public class FormTest {
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import d_07_02_22_FormPage.FormPage;
+
+
+
+public class FormTest {
+	
+	private WebDriver driver;
+	private FormPage fp;
+
+	@BeforeMethod
+	public void beforeMethod() {
+		
+		System.setProperty("webdriver.chrome.driver", "driver-lib/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+		fp=new FormPage (driver);
+		
+		driver.get("file:///C:/Users/Vandot/Downloads/form.html");
+			driver.manage().window().maximize();
+		
+	}
+
+	@Test
+	public void Test() throws InterruptedException {
+		fp.getInputName().sendKeys("Nada Nikolic");
+		fp.getInputGender("female").click();
+		fp.getInputDateOfBirth().sendKeys("24.06.1983.");
+		Thread.sleep(3000);
+		fp.getInputEmail().sendKeys("dshfd@gdsg.ash");
+		
+		Thread.sleep(3000);
+		fp.Role().selectByValue("QA");
+		
+		Thread.sleep(3000);
+		fp.getCheckBoxInputs("online_courses").click();
+		Thread.sleep(3000);
+		fp.getInputComment().sendKeys(":D :P :)"); 
+		Thread.sleep(3000);
+		fp.getSubmitButton().click();
+		Thread.sleep(3000);
+		fp.daLiSuPodaciSacuvani();
+	
+	}
+	
+	
+	@AfterMethod
+	public void afterMethod() {
+		driver.quit();
+	}
 }
